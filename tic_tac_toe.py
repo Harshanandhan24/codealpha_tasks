c7d2d3436b3dea848e24b9a5710eac5623cf2fa9
Harshanandhan24 <harshanandhangoud@gmail.com>
2026-06-08 19:07:00 +0530

    Add files via upload

---
+++ tic_tac_toe.py
@@ -0,0 +1,52 @@
+import random
+
+board = [" " for x in range(9)]
+
+def print_board():
+    print()
+    print(board[0] + "|" + board[1] + "|" + board[2])
+    print("-+-+-")
+    print(board[3] + "|" + board[4] + "|" + board[5])
+    print("-+-+-")
+    print(board[6] + "|" + board[7] + "|" + board[8])
+    print()
+
+def check_winner(player):
+    wins = [
+        [0,1,2],[3,4,5],[6,7,8],
+        [0,3,6],[1,4,7],[2,5,8],
+        [0,4,8],[2,4,6]
+    ]
+
+    for combo in wins:
+        if all(board[i] == player for i in combo):
+            return True
+    return False
+
+while True:
+    print_board()
+
+    move = int(input("Enter position (1-9): ")) - 1
+
+    if board[move] == " ":
+        board[move] = "X"
+    else:
+        print("Position already taken!")
+        continue
+
+    if check_winner("X"):
+        print_board()
+        print("You win!")
+        break
+
+    if " " not in board:
+        print("Draw!")
+        break
+
+    ai_move = random.choice([i for i in range(9) if board[i] == " "])
+    board[ai_move] = "O"
+
+    if check_winner("O"):
+        print_board()
+        print("AI wins!")
+        break
